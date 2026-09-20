37a8adc67b67dd099cea67b12da16c66b6fadda6
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-09-20 19:27:33 +0100

    removed undefined import.

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from discord import CustomActivity, Status, app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from bot import bot, OutBot
 from utils import ERROR_MESSAGE
-
 
 
 load_dotenv("config/.env")
