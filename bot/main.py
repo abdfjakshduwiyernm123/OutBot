4934b7241676077236987ca399d41336b9d84174
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-09-21 20:10:01 +0100

    Security update

--- bot/main.py
+++ bot/main.py
@@ -6,8 +6,6 @@ from bot import load_env_bot_token, OutBot
 
 
 if __name__ == "__main__":
-    discord_token = load_env_bot_token()
-
     bot = OutBot(
         activity=discord.Game(name="📖 Reading Documentation"),
         command_prefix="NONE",
@@ -16,6 +14,6 @@ if __name__ == "__main__":
     )
 
     try:
-        bot.run(discord_token)
+        bot.run(load_env_bot_token())
     except discord.LoginFailure:
         raise RuntimeError("The Discord bot token in config/.env is invalid.")
